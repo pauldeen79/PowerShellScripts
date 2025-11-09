665c237229198c40bc9296412c41444aa23452eb
Paul Deen <paul.deen@dutchworkz.nl>
2025-11-09 16:56:19 +0100

    example of async methods without Async suffix

--- FindInFiles.ps1
+++ FindInFiles.ps1
@@ -1,10 +1,10 @@
 # Example how to find lines that match multiple conditions
 
-Get-ChildItem -Path "D:\Git\CrossCutting\src" -Recurse -Filter *.cs | 
+Get-ChildItem -Path "D:\Git\ClassFramework\src" -Recurse -Filter *.cs | 
     ForEach-Object {
         $name = $_.FullName
         Get-Content $name | 
-        # Where-Object { $_ -match ' Task<' -and $_ -notmatch 'Async' -and $_ -notmatch 'Task.FromResult' -and $_ -notmatch 'Func<'} |
-        Where-Object { $_ -match '\.Add' -and $_ -match 'Async' -and $_ -notmatch '() =>' } |
+        Where-Object { $_ -match ' Task<' -and $_ -notmatch 'Async\(' -and $_ -notmatch 'Async<' -and $_ -notmatch 'Task.FromResult' -and $_ -notmatch 'Func<'} |
+        # Where-Object { $_ -match '\.Add' -and $_ -match 'Async' -and $_ -notmatch '() =>' } |
         ForEach-Object { "$($name): $_" }
     }
