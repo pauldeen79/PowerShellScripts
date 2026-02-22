7a5de4e9aef421d816bffbcbccada2e1054fbabe
Paul Deen <pauldeen@gmx.li>
2026-02-22 10:54:16 +0100

    Added some more examples in FindInFiles script

--- FindInFiles.ps1
+++ FindInFiles.ps1
@@ -1,10 +1,11 @@
 # Example how to find lines that match multiple conditions
 
-Get-ChildItem -Path "D:\Git\ClassFramework\src" -Recurse -Filter *.cs | 
+Get-ChildItem -Path "C:\Workspace" -Recurse -Filter *.csproj | 
     ForEach-Object {
         $name = $_.FullName
         Get-Content $name | 
-        Where-Object { $_ -match ' Task<' -and $_ -notmatch 'Async\(' -and $_ -notmatch 'Async<' -and $_ -notmatch 'Task.FromResult' -and $_ -notmatch 'Func<'} |
+        # Where-Object { $_ -match ' Task<' -and $_ -notmatch 'Async\(' -and $_ -notmatch 'Async<' -and $_ -notmatch 'Task.FromResult' -and $_ -notmatch 'Func<'} |
         # Where-Object { $_ -match '\.Add' -and $_ -match 'Async' -and $_ -notmatch '() =>' } |
+        Where-Object { $_ -cmatch 'CsharpExpressionDumper' }
         ForEach-Object { "$($name): $_" }
     }
